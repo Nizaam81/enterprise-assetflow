92c3cceb63967f05d1b81b40e8c9b798956e524a
agnel <agnelvincent779@gmail.com>
2026-07-17 14:16:38 +0530

    feat: updated frontend auth service base URL

--- Front-end/src/services/authService.js
+++ Front-end/src/services/authService.js
@@ -2,7 +2,6 @@ import api from './axios.js';
 
 export const authService = {
   /**
-   * Register a new employee
    * @param {Object} userData - The user registration data
    * @returns {Promise} Axios response promise
    */
@@ -11,11 +10,7 @@ export const authService = {
       const response = await api.post('/employ-signup', userData);
       return response.data;
     } catch (error) {
-      // You can add custom error formatting here if needed
       throw error.response?.data || error;
     }
   },
-  
-  // You would add other auth calls here later, e.g.:
-  // login: async (credentials) => { ... }
 };
