0275454153fe8b67a3b8fc0daad0594e16ed28cd
mn 81 <mnizamudheen81@gmail.com>
2026-07-17 11:43:04 +0530

    Revert "test:add-cross"
    This reverts commit d41d190477d0e191b8c166738f2aa7dfc864622d.

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -1,23 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
 
 // Connect to MongoDB
 connectDB();
 
 app.use(express.json());
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
