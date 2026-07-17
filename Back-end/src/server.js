1692055f81cd6fecbd62b71a23ebee0f4872007f
mn 81 <mnizamudheen81@gmail.com>
2026-07-17 11:50:38 +0530

    fix:-fixed-cross

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
-// Connect to MongoDB
 connectDB();
 
 app.use(express.json());
